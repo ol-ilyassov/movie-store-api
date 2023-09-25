18a6887bb763e6e02dfd8a4e73553eb25c37c0fe
Olzhas Ilyassov <olzhas.ilyassov@alg-in.kz>
2023-09-26 05:46:10 +0600

    add update action on movie model

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -93,7 +93,20 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 
 // Update
 func (m MovieModel) Update(movie *Movie) error {
-	return nil
+	query := `
+UPDATE movies
+SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+WHERE id = $5
+RETURNING version`
+	args := []any{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID,
+	}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
 }
 
 // Delete
