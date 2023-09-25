f798c5926b46c4db0d9d3fbc9c6aa4faf758044d
Olzhas Ilyassov <olzhas.ilyassov@alg-in.kz>
2023-09-26 05:28:16 +0600

    add models holder for application

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,8 @@ import (
 	"os"
 	"time"
 
+	"movie.api/internal/data"
+
 	_ "github.com/lib/pq"
 )
 
@@ -29,6 +31,7 @@ type config struct {
 type application struct {
 	config config
 	logger *slog.Logger
+	models data.Models
 }
 
 func main() {
@@ -63,6 +66,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	// * Application setup.
