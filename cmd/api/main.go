7a15ab2068ff4c18b2e3480a4185fc8800dabc67
Olzhas Ilyassov <olzhas.ilyassov@alg-in.kz>
2023-09-26 04:49:00 +0600

    add PostgreSQL connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +18,12 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  time.Duration
+	}
 }
 
 type application struct {
@@ -23,18 +33,39 @@ type application struct {
 
 func main() {
 	var cfg config
+	var err error
 
+	// * Flags setup.
 	flag.IntVar(&cfg.port, "port", 8100, "server port")
 	flag.StringVar(&cfg.env, "env", "development", "environment (development|staging|production)")
+
+	// os.Getenv("MOVIES.API_DB_DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://movies_api:pa55word@localhost/movies_api", "PostgreSQL DSN")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
+
 	flag.Parse()
 
+	// * Logger setup.
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
+	// * Database connection setup.
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+	logger.Info("database connection pool established")
+
+	// * Configurations setup.
 	app := &application{
 		config: cfg,
 		logger: logger,
 	}
 
+	// * Application setup.
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
 		Handler:      app.routes(),
@@ -45,7 +76,34 @@ func main() {
 	}
 	logger.Info("server launch", "addr", srv.Addr, "env", cfg.env)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+// openDB returns a sql.DB connection pool.
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// maximum of open (in-use + idle) connections.
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	// maximum of idle connections.
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+	// maximum idle timeout for connections.
+	db.SetConnMaxIdleTime(cfg.db.maxIdleTime)
+
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		5*time.Second,
+	)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
