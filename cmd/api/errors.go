ab3a822489a8d945b95860c439f0f6e53509abb9
Olzhas Ilyassov <olzhas.ilyassov@alg-in.kz>
2023-09-26 04:14:38 +0600

    add errors helper methods

---
+++ cmd/api/errors.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// logError method is a generic helper for logging an error message along with the current request method and URL as attributes in the log entry.
+func (app *application) logError(r *http.Request, err error) {
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+	)
+	app.logger.Error(err.Error(), "method", method, "uri", uri)
+}
+
+// errorResponse method is a generic helper for sending JSON-formatted error messages to the client with a given status code.
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
+	env := envelope{"error": message}
+
+	err := app.writeJSON(w, status, env, nil)
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(500)
+	}
+}
+
+// serverErrorResponse method will be used when our application encounters an unexpected problem at runtime.
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logError(r, err)
+	message := "the server encountered a problem and could not process your request"
+	app.errorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+// badRequestResponse method will be used to send a 400 Bad Request status code and JSON response to the client.
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+}
+
+// notFoundResponse method will be used to send a 404 Not Found status code and JSON response to the client.
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	message := "the requested resource could not be found"
+	app.errorResponse(w, r, http.StatusNotFound, message)
+}
+
+// methodNotAllowedResponse method will be used to send a 405 Method Not Allowed status code and JSON response to the client.
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
+
+// failedValidationResponse() method will be used to send a 422 Unprocessable Entity status code and JSON response with errors set to the client.
+func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
