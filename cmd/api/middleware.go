48d37efe1d2b454e4198aaa0a05e14ee4f0542d5
Olzhas Ilyassov <olzhas.ilyassov@alg-in.kz>
2023-09-29 12:44:10 +0600

    add permissions check middleware method

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -163,3 +163,25 @@ func (app *application) requireActivatedUser(next http.HandlerFunc) http.Handler
 	return app.requireAuthenticatedUser(fn)
 	// activated and authenticated.
 }
+
+// requirePermission
+func (app *application) requirePermission(code string, next http.HandlerFunc) http.HandlerFunc {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		user := app.contextGetUser(r)
+
+		permissions, err := app.models.Permissions.GetAllForUser(user.ID)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+
+		if !permissions.Include(code) {
+			app.notPermittedResponse(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	}
+	// Wraps chain, to avoid extra definition to call already required middlewares.
+	return app.requireActivatedUser(fn)
+}
